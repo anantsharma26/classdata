42e9a291059ad0c1491a599305dc5561998b19ff
arjunSharma <anant101sharma@gmail.com>
2024-01-31 22:50:33 +0530

    Add Class section completed

--- ClassData/custom.js
+++ ClassData/custom.js
@@ -4,6 +4,14 @@ const addnewform = document.querySelector(".addnewclass");
 const classname = document.querySelector("#clname");
 const classid = document.querySelector("#clid");
 const mainTable = document.querySelector(".main_table tbody");
+const editform = document.querySelector(".editclassform");
+const editformclass = document.querySelector(".editclass");
+const editName = document.querySelector("#editname");
+const editId = document.querySelector("#editid");
+
+
+
+
 const newdata = [];
 
 addclassbtn.addEventListener("click",(e)=>{
@@ -11,22 +19,60 @@ addclassbtn.addEventListener("click",(e)=>{
     classform.classList.add("clickedvmro");
 })
 
-addnewform.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const namedata =  classname.value;
-    const nameid =  classid.value;
-    newdata.push({className:namedata,classId:nameid,studentno:0});
-    classname.value = "";
-    classid.value = "";
-    mainTable.innerHTML = `<td>${newdata.className}</td>
-    <td>${newdata.studentno}</td>
-    <td>${newdata.classId}</td>
-    <td><button id="edit" class="btn btn-info">Edit</button></td>
-    <td><button id="del" class="btn btn-danger">Delete</button></td>`;
-})
+
 
 mainTable.addEventListener("click",(e)=>{
     if(e.target.classList.contains("btn-danger")){
         e.target.parentNode.parentNode.remove();
     }
+})
+
+function addtableData(){
+    mainTable.innerHTML = ""; 
+    for(data of newdata){
+        mainTable.innerHTML += `<tr><td>${data.className}</td>
+        <td>${data.studentno}</td>
+        <td>${data.classId}</td>
+        <td><button id="edit" data-id="${data.classId}" class="btn btn-info editbtn">Edit</button></td>
+        <td><button id="del" data-id="${data.classId}" class="btn btn-danger delbtn">Delete</button></td></tr>`;
+    }
+}
+
+
+
+addnewform.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const namedata =  classname.value;
+    const nameid =  classid.value;
+    newdata.push({className:namedata,classId:nameid,studentno:0,});
+    classname.value = "";
+    classid.value = "";   
+    addtableData();
+    let editbtn = document.querySelectorAll(".editbtn");
+    editbtn.forEach(function(elem) {
+    elem.addEventListener("click", function(e) {
+        const eachid = e.target.getAttribute("data-id");
+        const index = newdata.findIndex(object => {
+            return object.classId === eachid;
+        });
+        editName.value = newdata[index].className;
+        editId.value = newdata[index].classId;
+        editform.classList.add("clickedvmro");
+    });
+    
+});
+
+});
+
+
+
+editformclass.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const newName = editName.value;
+    const newId = editId.value;
+    const newindex = newdata.findIndex(object =>{
+       return object.classId === newId;
+    })
+    newdata[newindex].className = newName;
+    addtableData();
 })
