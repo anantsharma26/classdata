744fe9dec75a66fe17f0817a7df56000fbcc935f
arjunSharma <anant101sharma@gmail.com>
2024-02-06 19:24:24 +0530

    login validations and current user data applied to the dashboard

--- ClassData/login.js
+++ ClassData/login.js
@@ -1,10 +1,13 @@
 const loginform = document.querySelector("#loginform");
+const loginId = document.querySelector("#loginid");
+const loginPswd = document.querySelector("#loginpswd");
+
 function initLogin() {
   const isLoggedIn = localStorage.getItem("loggedInUser");
   console.log("isLoggedIn", isLoggedIn);
   if (isLoggedIn) {
     window.location.href =
-      "http://127.0.0.1:5500/classdata/ClassData/index.html";
+      "C:/Users/dell/Downloads/practical/classdata/ClassData/index.html";
   }
 }
 
@@ -12,17 +15,29 @@ initLogin();
 loginform.addEventListener("submit", (e) => {
   e.preventDefault();
   //TODO Add check to see if the entered email and password are matching the StudentData in localStorage
+  const logInId = loginId.value;
+  const logInPswd = loginPswd.value;
+  const localstuData = JSON.parse(localStorage.getItem("studentData"));
+  let obj = localstuData.find(o => o.stuMail === logInId);
+  if(logInId != obj.stuMail){
+    alert("wrong email");
+  }
+  if(logInPswd != obj.stuPass){
+    alert("wrong Password");
+  } 
+ else{  
   localStorage.setItem(
     "loggedInUser",
     //TODO replace this data with curent logged in user data
     JSON.stringify({
-      stuName: "Sukhraj Singh",
-      stuId: "123456",
-      stuRoll: "387487",
-      stuMail: "sukhrajsonu98@gmail.com",
-      stuPass: "Sukhraj`@123",
-      stuclId: "123456",
+      stuName: obj.stuName,
+      stuId: obj.stuId,
+      stuRoll: obj.stuRoll,
+      stuMail: obj.stuMail,
+      stuPass: obj.stuPass,
+      stuclId: obj.stuclId,
     })
   );
-  window.location.href = "http://127.0.0.1:5500/classdata/ClassData/index.html";
+  }
+  window.location.href = "C:/Users/dell/Downloads/practical/classdata/ClassData/index.html";
 });
--- ClassData/main.js
+++ ClassData/main.js
@@ -1,9 +1,10 @@
 const logoutButton = document.querySelector("#logoutBtn");
 const userTitle = document.querySelector("#userName");
+const userEmail = document.querySelector("#userEmail");
 
 logoutButton.addEventListener("click", (e) => {
   localStorage.removeItem("loggedInUser");
-  window.location.href = "http://127.0.0.1:5500/classdata/ClassData/login.html";
+  window.location.href = "C:/Users/dell/Downloads/practical/classdata/ClassData/login.html";
 });
 // TODO Add check to redirect user to login page if isLoggedIn is not found in localStorage
 // TODO Add user email to the top of page if isLoggedIn
@@ -14,5 +15,6 @@ function loadUserDetails() {
   //   console.log("Loading user details...", isLoggedIn);
   if (isLoggedIn) {
     userTitle.textContent = isLoggedIn.stuName;
+    userEmail.textContent = isLoggedIn.stuMail;
   }
 }
